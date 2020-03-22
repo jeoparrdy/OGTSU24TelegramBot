c258970c53b6ac144fb78fa21cbe10078d7f9118
jeoparrdy <46939348+jeoparrdy@users.noreply.github.com>
2020-03-22 21:24:52 +0200

    new

---
+++ src/main/java/Bot.java
@@ -0,0 +1,34 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
+
+import java.util.List;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    public void onUpdatesReceived(List<Update> updates) {
+
+    }
+
+    public String getBotUsername() {
+        return null;
+    }
+
+    public String getBotToken() {
+        return null;
+    }
+}

5d966205fb96750421bacb614f0e81a2d5a60707
jeoparrdy <jeoparrdy@gmail.com>
2020-03-23 12:05:45 +0200

    new 2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -1,7 +1,10 @@
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 import java.util.List;
@@ -16,8 +19,35 @@ public class Bot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
-    public void onUpdateReceived(Update update) {
 
+    public void sendMsg(Message message, String text){
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(message.getChatId().toString());
+        sendMessage.setReplyToMessageId(message.getMessageId());
+        sendMessage.setText(text);
+        try{
+            execute(sendMessage);
+        }catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public void onUpdateReceived(Update update) {
+        Message message = update.getMessage();
+        if (message != null && message.hasText()){
+            switch (message.getText()){
+                case "/help":
+                    sendMsg(message,"");
+                    break;
+                case "/info":
+                    sendMsg(message,"");
+                    break;
+                default:
+
+            }
+        }
     }
 
     public void onUpdatesReceived(List<Update> updates) {
@@ -25,10 +55,10 @@ public class Bot extends TelegramLongPollingBot {
     }
 
     public String getBotUsername() {
-        return null;
+        return "OGTSU24";
     }
 
     public String getBotToken() {
-        return null;
+        return "958714286:AAF0tbujB8RZ7Rec7AF6RLWzz_X58jqLN20";
     }
 }
